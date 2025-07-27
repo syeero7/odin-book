b2bb431a5e195ff76dabc5a433554b04665396da
Emal <EmalHasitha@proton.me>
2025-07-28 04:22:52 +0530

    feat: add navigation loading indicator

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import AuthProvider from "@/components/AuthProvider";
 import Navbar from "@/components/Navbar";
+import Fallback from "@/components/Fallback";
 
 function App() {
+  const navigation = useNavigation();
+
   return (
     <AuthProvider>
+      {navigation.state !== "idle" && <Fallback />}
       <Navbar />
       <Outlet />
     </AuthProvider>
